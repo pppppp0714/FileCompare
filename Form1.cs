f5acccc3306fc4b8ed640c5634b4024b56fda83a
지혁 박 <jhboy0714@gmail.com>
2026-04-16 12:29:56 +0900

    과제2 코딩과 테스트 완료 폴더를 선택하고 ListView에 파일 표시 방식을 바꿔 수정된 파일, 기존 파일, 새로운 파일 등을 한 눈에 확인할 수 있도록 기능 추가

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,8 @@
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using ListView = System.Windows.Forms.ListView;
+using System.IO;
+using System.Linq;
+using System.Drawing;
 
 namespace FileCompare
 {
@@ -8,6 +11,69 @@ namespace FileCompare
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CompareListViews()
+        {
+            // 오른쪽 목록 항목들을 이름별로 빠르게 찾기 위한 딕셔너리 구성
+            var rightDict = new Dictionary<string, ListViewItem>(StringComparer.OrdinalIgnoreCase);
+            foreach (ListViewItem r in lvwRightDir.Items)
+            {
+                rightDict[r.Text] = r;
+            }
+
+            // 왼쪽 항목 처리
+            foreach (ListViewItem l in lvwLeftDir.Items)
+            {
+                if (!rightDict.TryGetValue(l.Text, out var r))
+                {
+                    // 오른쪽에 없음 -> 추가된 항목(왼쪽에만 존재하는 항목으로 처리)
+                    l.ForeColor = Color.Purple; // 보라
+                }
+                else
+                {
+                    // 양쪽에 모두 존재 -> Tag의 LastWriteTime으로 비교
+                    if (l.Tag is FileSystemInfo lfi && r.Tag is FileSystemInfo rfi)
+                    {
+                        var lTime = lfi.LastWriteTime;
+                        var rTime = rfi.LastWriteTime;
+                        if (lTime > rTime)
+                        {
+                            // 왼쪽이 더 최신 -> 수정됨(왼쪽이 새로움)
+                            l.ForeColor = Color.Red;
+                            r.ForeColor = Color.Gray; // 기존(더이전)
+                        }
+                        else if (lTime < rTime)
+                        {
+                            // 오른쪽이 더 최신 -> 수정됨(오른쪽이 새로움)
+                            r.ForeColor = Color.Red;
+                            l.ForeColor = Color.Gray;
+                        }
+                        else
+                        {
+                            // 동일함
+                            l.ForeColor = Color.Black;
+                            r.ForeColor = Color.Black;
+                        }
+                    }
+                    else
+                    {
+                        // Tag가 설정되지 않았으면 검정색으로 유지
+                        l.ForeColor = Color.Black;
+                        r.ForeColor = Color.Black;
+                    }
+                }
+            }
+
+            // 왼쪽에 없는 오른쪽 전용 항목
+            var leftNames = new HashSet<string>(lvwLeftDir.Items.Cast<ListViewItem>().Select(i => i.Text), StringComparer.OrdinalIgnoreCase);
+            foreach (ListViewItem r in lvwRightDir.Items)
+            {
+                if (!leftNames.Contains(r.Text))
+                {
+                    r.ForeColor = Color.Purple; // 오른쪽에만 존재 -> 추가된 항목
+                }
+            }
         }
         private void PopulateListView(ListView lv, string folderPath)
         {
@@ -23,6 +89,9 @@ namespace FileCompare
                     var item = new ListViewItem(d.Name);
                     item.SubItems.Add("<DIR>");
                     item.SubItems.Add(d.LastWriteTime.ToString("g"));
+                    // store FileSystemInfo for later comparison and default color
+                    item.Tag = d;
+                    item.ForeColor = Color.Black;
                     lv.Items.Add(item);
                 }
                 // 파일추가
@@ -34,6 +103,9 @@ namespace FileCompare
                     var item = new ListViewItem(f.Name);
                     item.SubItems.Add(f.Length.ToString("N0") + " 바이트");
                     item.SubItems.Add(f.LastWriteTime.ToString("g"));
+                    // store FileSystemInfo for later comparison and default color
+                    item.Tag = f;
+                    item.ForeColor = Color.Black;
                     lv.Items.Add(item);
                 }
                 // 컬럼너비자동조정(컨텐츠기준)
@@ -90,6 +162,7 @@ namespace FileCompare
                 {
                     txtLeftDir.Text = dlg.SelectedPath;
                     PopulateListView(lvwLeftDir, dlg.SelectedPath);
+                    CompareListViews();
                 }
 
             }
@@ -110,6 +183,7 @@ namespace FileCompare
                 {
                     txtRightDir.Text = dlg.SelectedPath;
                     PopulateListView(lvwRightDir, dlg.SelectedPath);
+                    CompareListViews();
                 }
 
             }
